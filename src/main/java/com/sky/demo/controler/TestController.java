5d3c3c49418044b46f02ad3a53ea3163f321f979
zouwensheng <zouwensheng@sky.com>
2023-07-29 13:13:33 +0800

    2023-07-29测试

--- src/main/java/com/sky/demo/controler/TestController.java
+++ src/main/java/com/sky/demo/controler/TestController.java
@@ -16,6 +16,6 @@ public class TestController {
     @GetMapping("/hello")
     public String test() {
         System.out.println("hello");
-        return "hello";
+        return "hello123";
     }
 }
